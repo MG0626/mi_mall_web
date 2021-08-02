0f1b4d6ead5f4f2c3e15a4e95165222ffc7a8891
cmg <1370636565@qq.com>
2021-08-02 19:57:52 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入axios
+import http from './network/request'
 
 Vue.config.productionTip = false
+
+// 添加到原型上
+Vue.prototype.$http = http;
 
 new Vue({
   router,
---
+++ src/network/request.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: '',
+  timeout: 5000
+});
+
+// 请求拦截器
+http.interceptors.request.use(config => {
+  return config;
+});
+
+// 响应拦截器
+http.interceptors.response.use(
+  res => {
+    return res;
+  },
+  err => {
+    return err.response;
+  }
+);
+
+export default http;
